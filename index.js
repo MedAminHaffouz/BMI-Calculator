c6fceb018c87d0eb1c660e1d2963dec7ccd32110
MedAminHaffouz <mohamedamin.haffouz@insat.ucar.tn>
2024-11-11 14:00:36 +0100

    First attempt to build

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 //console.log('Happy developing ✨')
 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let w = prompt("Give weight:");
+    let h = prompt("Give height:");
+
+    let bmi = w / (h * h);
+    bmi=bmi.toFixed(2);
+    let paragraph = document.getElementById("paragraph");
+    let res = "Your BMI is " + bmi;
+
+    // Use double quotes around the style value and avoid curly braces
+    paragraph.innerHTML = "<p style='color:red;'>" + res + "</p>";
+});
